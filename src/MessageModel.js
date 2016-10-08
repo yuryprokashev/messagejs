7f17a1c5ddbbd8b26f96c4875de681c78649e137
Yury Prokashev <yury.prokashev@gmail.com>
2016-10-08 15:49:00 +0300

    DB_MESSAGES does not connect

--- src/MessageModel.js
+++ src/MessageModel.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 // mongoose.connect("mongodb://localhost:27017/pfin");
-mongoose.connect(process.env.DB_MESSAGES);
+mongoose.connect(`${process.env.DB_MESSAGES}`);
 
 var PayloadModel = mongoose.model('Payload', require('./message.schema.js'), 'messages');
 
