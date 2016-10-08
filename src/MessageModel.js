2e1dc3397acbd4de8109b0b541fa394f65793c32
Yury Prokashev <yury.prokashev@gmail.com>
2016-10-08 20:48:33 +0300

    connect to DB

--- src/MessageModel.js
+++ src/MessageModel.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 
 // mongoose.connect("mongodb://localhost:27017/pfin");
 console.log(process.env.DB_MESSAGES);
-mongoose.connect(`mongodb://pfdb.edufun.me:27107/pfin`);
+mongoose.connect("mongodb://54.229.108.38:27017/pfin");
 
-var PayloadModel = mongoose.model('Payload', require('./message.schema.js'), 'messages');
+var MessageModel = mongoose.model('Message', require('./message.schema.js'), 'messages');
 
-module.exports = PayloadModel;
+module.exports = MessageModel;
