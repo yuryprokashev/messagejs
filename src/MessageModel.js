713d30d88a3b077cef80432bfc4de841249eaf21
Yury Prokashev <yury.prokashev@gmail.com>
2016-10-08 15:52:54 +0300

    one more try

--- src/MessageModel.js
+++ src/MessageModel.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 // mongoose.connect("mongodb://localhost:27017/pfin");
+console.log(process.env.DB_MESSAGES);
 mongoose.connect(`${process.env.DB_MESSAGES}`);
 
 var PayloadModel = mongoose.model('Payload', require('./message.schema.js'), 'messages');
