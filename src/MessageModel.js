78980d9f9e3446192683636b78655d89a4df199e
Yury Prokashev <yury.prokashev@gmail.com>
2016-10-08 15:56:37 +0300

    +1 try

--- src/MessageModel.js
+++ src/MessageModel.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 // mongoose.connect("mongodb://localhost:27017/pfin");
 console.log(process.env.DB_MESSAGES);
-mongoose.connect(`${process.env.DB_MESSAGES}`);
+mongoose.connect(`${process.env.DB_MESSAGES}:27107/pfin`);
 
 var PayloadModel = mongoose.model('Payload', require('./message.schema.js'), 'messages');
 
