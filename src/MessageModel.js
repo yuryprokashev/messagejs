6bb4019ef5de7a878962889a3ebac5b1d7a6a1bd
Yury Prokashev <yury.prokashev@gmail.com>
2016-10-10 09:32:38 +0300

    switch to DEV environment

--- src/MessageModel.js
+++ src/MessageModel.js
@@ -1,8 +1,9 @@
 var mongoose = require('mongoose');
 
-// mongoose.connect("mongodb://localhost:27017/pfin");
-// console.log(process.env.DB_MESSAGES);
-mongoose.connect("mongodb://54.229.108.38:27017/pfin");
+const MONGO_HOST = "mongodb://localhost:27017/pfin";
+// const MONGO_HOST = "mongodb://54.229.108.38:27017/pfin";
+
+mongoose.connect(MONGO_HOST);
 
 var MessageModel = mongoose.model('Message', require('./message.schema.js'), 'messages');
 
